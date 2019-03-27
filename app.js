28eb63edf90a0252b6e2ab7aaa44e7977258677e
Dan Bryk <danbryk@icloud.com>
2019-03-27 14:55:44 -0400

    commit completed DOM pt. 2 exercise work amend to restore appending MDL classes in .js

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 // dan bryk - bloc - to-do-app assignment pt. 2 - mar 27/19
 
 function onReady() {
-  const ADD_TODO_FORM = document.getElementById('addToDoForm');
-  const NEW_TODO_TEXT = document.getElementById('newToDoText');
-  const TODO_LIST = document.getElementById('toDoList');
+  const toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
 
-  ADD_TODO_FORM.addEventListener('submit', (event) => {
-    // console.log(event);
-    // console.log(this);
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      checkbox.classList.add('mdl-checkbox__input'); // add MDL class to checkbox
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.classList.add('mdl-list_item'); // add MDL class to <li>
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    let title = NEW_TODO_TEXT.value; // get text
-    let newLi = document.createElement('li'); // create a new <li>
-
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = "Delete";
-
-    deleteButton.addEventListener('click', function(event) {
-        TODO_LIST.removeChild(this.parentElement); // remove the <li>
-    })
-
-    deleteButton.classList.add('mdl-button');
-    deleteButton.classList.add('mdl-js-button'); // add mdl classes to delete button
-
-    newLi.textContent = title; // set the title
-    // let checkbox = document.createElement('input'); // create a new input
-    let checkbox = document.createElement('input'); // create a new input
-    checkbox.type = "checkbox"; // set the input item's type to checkbox
-
-    checkbox.classList.add('mdl-checkbox__input'); // add mdl class to the checkbox
-
-    newLi.appendChild(checkbox); // attach checkbox to the <li>
-
-    newLi.appendChild(deleteButton); // attach delete button to the <li>
-
-    newLi.classList.add('mdl-list__item'); // add mdl class to the li
-
-    TODO_LIST.appendChild(newLi); // attach the li to the ul
-
-    NEW_TODO_TEXT.value = ''; // empty form field for next input
-
+    createNewToDo();
   });
+
+  renderTheUI();
 }
 
 window.onload = function() {
