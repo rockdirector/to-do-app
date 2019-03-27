adf52b691d10f2257fcd48ec4d440964c3f67e01
Dan Bryk <danbryk@icloud.com>
2019-03-27 16:54:29 -0400

    commit completed DOM pt. 2 assignment work

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 // dan bryk - bloc - to-do-app assignment pt. 2 - mar 27/19
 
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-
-  function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -27,12 +15,42 @@ function onReady() {
       checkbox.type = "checkbox";
       checkbox.classList.add('mdl-checkbox__input'); // add MDL class to checkbox
 
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
+
+      deleteButton.addEventListener('click', function(event) {
+        event.preventDefault();
+            toDos = toDos.filter(function(item) {
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-      newLi.classList.add('mdl-list_item'); // add MDL class to <li>
+      newLi.appendChild(deleteButton);
+
+      newLi.classList.add('mdl-list_item'); // add MDL class to entire <li>
+
     });
+
+  }
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id,
+    });
+
+    newToDoText.value = '';
+    id++; // increment id
+    console.log("id: " + id);
+    renderTheUI();
   }
 
   addToDoForm.addEventListener('submit', event => {
